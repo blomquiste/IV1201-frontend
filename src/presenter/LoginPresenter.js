d5dad3797212617c5847f8fe921bf12fd73dc002
vrncndlr <veronica.j.nadler@gmail.com>
2024-03-07 20:03:03 +0100

    cleaning

--- src/presenter/LoginPresenter.js
+++ src/presenter/LoginPresenter.js
@@ -1,26 +1,25 @@
-import React, { useState} from "react";
+import React from "react";
+import { useNavigate } from 'react-router-dom';
 import LoginView from "../view/LoginView";
 import FailedLoginView from "../view/FailedLoginView"
-import User from "./UserPresenter";
-import NavigationBar from "../components/NavigationBar"
-import Overview from "./OverviewPresenter";
 
 /**
  * Handles logic for login-related views
- * @param {Object} params
+ * @param props
  * @param {boolean} props.loggedIn - true if user is logged in
  * @param {boolean} props.failedLogin - true if the user has tried to log in and failed
- * @param {function} props.callDB - login function that calls the backend api to login user
+ * @param {function} props.handleLogin - login function that calls the backend api to login user
  * @returns LoginView - view that shows the login page
  * FailedLoginView - shows the LoginView with added error message about failed login
- * UserView - landing page after succesful login
+ * UserView - landing page after successful login
  */
 export default function Login(props) {
+    const navigate = useNavigate();
+
     return (<>
             <div>{!props.loggedIn && !props.failedLogin && <LoginView onLogin={props.handleLogin}/>}</div>
-            <div>{props.loggedIn && !props.recruiter && <User user={props.user}/>}</div>
-            <div>{props.loggedIn && props.recruiter && <Overview user={props.user}/>}</div>
-
+            <div>{props.loggedIn && !props.recruiter && navigate("/user")}</div>
+            <div>{props.loggedIn && props.recruiter && navigate("/overview")}</div>
             <div>{props.failedLogin && <FailedLoginView onLogin={props.handleLogin}/>}</div>
         </>
     )
